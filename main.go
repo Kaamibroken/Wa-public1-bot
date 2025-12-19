675a9445af4bc99b9d10ff463fdf1c4b7822683a
marslan-786 <marslansalfias@gmail.com>
2025-12-20 00:26:40 +0500

    Add files via upload

--- main.go
+++ main.go
@@ -3,15 +3,29 @@ package main
 import (
 	"compress/flate"
 	"compress/gzip"
+	"context"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
-	"strings"
+	"os/signal"
+	"strings" // اب یہ یو آر ایل چیک کرنے کے لیے استعمال ہو رہا ہے
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
 )
 
-// ریلوے یا لوکل انوائرمنٹ سے پورٹ اٹھانا
+var client *whatsmeow.Client
+
+// ریلوے پورٹ مینجمنٹ
 func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -20,88 +34,116 @@ func getPort() string {
 	return port
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// Query Parameter سے 'type' نکالنا
-	dataType := r.URL.Query().Get("type")
-	if dataType == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, `{"error": "Missing ?type parameter"}`)
-		return
-	}
-
+// آپ کا فراہم کردہ ڈیٹا فیچر (Node.js سے Go میں کنورٹڈ)
+func handleFetch(c *gin.Context) {
+	dataType := c.Query("type")
 	var targetURL string
 	var referer string
 
-	// یو آر ایل اور ریفرر سیٹ کرنا جیسا آپ کے JS کوڈ میں تھا
 	if dataType == "numbers" {
-		targetURL = "http://217.182.195.194/ints/agent/res/data_smsnumbers.php?frange=&fclient=&sEcho=2&iColumns=8&sColumns=%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=false&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&_=1766171476582"
+		targetURL = "http://217.182.195.194/ints/agent/res/data_smsnumbers.php?..." 
 		referer = "http://217.182.195.194/ints/agent/MySMSNumbers"
 	} else if dataType == "sms" {
-		targetURL = "http://217.182.195.194/ints/agent/res/data_smscdr.php?fdate1=2025-12-19%2000:00:00&fdate2=2085-12-19%2023:59:59&frange=&fclient=&fnum=&fcli=&fgdate=&fgmonth=&fgrange=&fgclient=&fgnumber=&fgcli=&fg=0&csstr=9df7a3e50debcd51cca20329b34c1bfe&sEcho=2&iColumns=9&sColumns=%2C%2C%2C%2C%2C%2C%2C%2C&iDisplayStart=0&iDisplayLength=-1&mDataProp_0=0&sSearch_0=&bRegex_0=false&bSearchable_0=true&bSortable_0=true&mDataProp_1=1&sSearch_1=&bRegex_1=false&bSearchable_1=true&bSortable_1=true&mDataProp_2=2&sSearch_2=&bRegex_2=false&bSearchable_2=true&bSortable_2=true&mDataProp_3=3&sSearch_3=&bRegex_3=false&bSearchable_3=true&bSortable_3=true&mDataProp_4=4&sSearch_4=&bRegex_4=false&bSearchable_4=true&bSortable_4=true&mDataProp_5=5&sSearch_5=&bRegex_5=false&bSearchable_5=true&bSortable_5=true&mDataProp_6=6&sSearch_6=&bRegex_6=false&bSearchable_6=true&bSortable_6=true&mDataProp_7=7&sSearch_7=&bRegex_7=false&bSearchable_7=true&bSortable_7=true&mDataProp_8=8&sSearch_8=&bRegex_8=false&bSearchable_8=true&bSortable_8=false&sSearch=&bRegex=false&iSortCol_0=0&sSortDir_0=desc&iSortingCols=1&_=1766171360378"
+		targetURL = "http://217.182.195.194/ints/agent/res/data_smscdr.php?..."
 		referer = "http://217.182.195.194/ints/agent/SMSCDRStats"
 	} else {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, `{"error": "Invalid type (use sms or numbers)"}`)
+		c.JSON(400, gin.H{"error": "Invalid type"})
 		return
 	}
 
-	// کلائنٹ ریکویسٹ بنانا
-	req, err := http.NewRequest("GET", targetURL, nil)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// وہی ہیڈرز جو آپ نے فراہم کیے تھے
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 13; V2040 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.7499.34 Mobile Safari/537.36")
-	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req, _ := http.NewRequest("GET", targetURL, nil)
+	req.Header.Set("User-Agent", "Mozilla/5.0...")
 	req.Header.Set("Accept-Encoding", "gzip, deflate")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	req.Header.Set("Cookie", "PHPSESSID=pb3620rtcrklvvrmndf8kmt93n")
 	req.Header.Set("Referer", referer)
+	req.Header.Set("Cookie", "PHPSESSID=pb3620rtcrklvvrmndf8kmt93n")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, `{"error": "Fetch failed", "details": "%s"}`, err.Error())
+		c.JSON(500, gin.H{"error": "Fetch failed"})
 		return
 	}
 	defer resp.Body.Close()
 
-	// ڈی کمپریشن ہینڈلنگ (Gzip/Deflate)
 	var reader io.ReadCloser
-	switch resp.Header.Get("Content-Encoding") {
-	case "gzip":
-		reader, err = gzip.NewReader(resp.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		defer reader.Close()
-	case "deflate":
+	// یہاں 'strings' کا استعمال ہو رہا ہے
+	encoding := resp.Header.Get("Content-Encoding")
+	if strings.Contains(encoding, "gzip") {
+		reader, _ = gzip.NewReader(resp.Body)
+	} else if strings.Contains(encoding, "deflate") {
 		reader = flate.NewReader(resp.Body)
-		defer reader.Close()
-	default:
+	} else {
 		reader = resp.Body
 	}
+	defer reader.Close()
 
-	// ڈیٹا پڑھنا اور واپس بھیجنا
-	body, err := io.ReadAll(reader)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.Write(body)
+	body, _ := io.ReadAll(reader)
+	c.Data(200, "application/json", body)
 }
 
 func main() {
-	port := getPort()
-	http.HandleFunc("/api/fetch", handleRequest)
+	dbLog := waLog.Stdout("Database", "INFO", true)
+	// سیشن ہینڈلنگ (Postgres)
+	container, err := sqlstore.New(context.Background(), "postgres", os.Getenv("DATABASE_URL"), dbLog)
+	if err != nil { panic(err) }
 
-	fmt.Printf("🚀 API Fetcher running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	deviceStore, err := container.GetFirstDevice(context.Background())
+	if err != nil { panic(err) }
+
+	client = whatsmeow.NewClient(deviceStore, waLog.Stdout("Client", "INFO", true))
+	client.AddEventHandler(eventHandler)
+
+	// ویب سرور سیٹ اپ
+	r := gin.Default()
+	r.StaticFile("/", "./web/index.html")
+	r.StaticFile("/pic.png", "./web/pic.png")
+	r.GET("/api/fetch", handleFetch)
+	
+	r.POST("/api/pair", func(c *gin.Context) {
+		var req struct{ Number string `json:"number"` }
+		c.BindJSON(&req)
+		client.Connect()
+		// پیرنگ کوڈ ریکویسٹ
+		code, _ := client.PairPhone(context.Background(), req.Number, true, whatsmeow.PairClientChrome, "Chrome (Linux)")
+		c.JSON(200, gin.H{"code": code})
+	})
+
+	go r.Run(":" + getPort())
+
+	if client.Store.ID != nil {
+		client.Connect()
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	client.Disconnect()
+}
+
+// واٹس ایپ MENU بٹن لاجک
+func sendOfficialMenu(chat types.JID) {
+	listMsg := &waProto.ListMessage{
+		Title:       proto.String("IMPOSSIBLE MENU"),
+		Description: proto.String("Select category"),
+		ButtonText:  proto.String("MENU"), // بٹن کا نام "MENU"
+		ListType:    waProto.ListMessage_SINGLE_SELECT.Enum(),
+		Sections: []*waProto.ListMessage_Section{
+			{
+				Title: proto.String("COMMANDS"),
+				Rows: []*waProto.ListMessage_Row{
+					{Title: proto.String("Ping"), RowID: proto.String("ping")},
+					{Title: proto.String("ID"), RowID: proto.String("id")},
+				},
+			},
+		},
+	}
+	client.SendMessage(context.Background(), chat, &waProto.Message{ListMessage: listMsg})
+}
+
+func eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		if v.Message.GetConversation() == "#menu" {
+			sendOfficialMenu(v.Info.Chat)
+		}
+	}
 }
